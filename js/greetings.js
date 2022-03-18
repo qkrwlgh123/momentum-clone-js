8b140cb95a08c97b3fbac5845b27df6fa7491eed
qkrwlgh123 <jiho1024@naver.com>
2022-03-18 16:53:42 +0900

    시계, 인사 부분 CSS로 위치 조정, 출력되는 인사 문구 시간대별로 다르게 출력되도록 수정

--- js/greetings.js
+++ js/greetings.js
@@ -16,7 +16,26 @@ function onLoginSubmit(event) {
 function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = `hello ${username}`;
+
+  // 시간이 오전(6시 ~ 12시)이면 -> `Good morning jiho`
+  // 시간이 점심이면(12시 ~ 2시) -> `Did you eat lunch, jiho?`
+  // 시간이 오후이면(2시 ~ 6시) -> `Good afternoon jiho`
+  // 시간이 저녁이면(6시 ~ 10시) -> `Have you eaten dinner, jiho?`
+  // 시간이 밤이면(10시 ~ 2시) -> `Good night jiho`
+  const date = new Date();
+  const hours = date.getHours();
+
+  if (hours >= 6 && hours < 12) {
+    greeting.innerText = `Good morning, ${username}`;
+  } else if (hours >= 12 && hours < 14) {
+    greeting.innerText = `Did you eat lunch, ${username}?`;
+  } else if (hours >= 14 && hours < 18) {
+    greeting.innerText = `Good afternoon, ${username}`;
+  } else if (hours >= 18 && hours < 22) {
+    greeting.innerText = `Have you eaten dinner, ${username}?`;
+  } else {
+    greeting.innerText = `Good night, ${username}`;
+  }
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
